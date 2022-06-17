b801ee59fb331cd34eed399bd6c9b4df71b34770
Fozil Zayn <fozilzayniddinov7777@gmail.com>
2022-06-18 02:45:22 +0500

    counter + hooks

--- examples/14-counter-for-hooks/src/Application.tsx
+++ examples/14-counter-for-hooks/src/Application.tsx
@@ -1,17 +1,41 @@
+import { ChangeEvent, useState } from "react";
+
+
+
+
 const Counter = () => {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
+
+  const handleReset = () => {
+    setCount(0)
+  }
+
+  const onChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+    setCount(+event.target.value)
+  }
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <p className="count">{count}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleReset}>Reset</button>
+        <button onClick={handleDecrement}>Decrement</button>
       </section>
       <section className="controls">
-        <form onSubmit={() => {}}>
+        <form onSubmit={() => { }}>
           <label htmlFor="set-to">Set Count</label>
-          <input id="set-to" type="number" />
+          <input id="set-to" type="number" value={count} onChange={onChangeInput} />
           <input type="submit" />
         </form>
       </section>
