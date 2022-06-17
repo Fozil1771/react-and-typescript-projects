04449f061630a535e25b8948a4adaba6cce547a3
Fozil Zayn <fozilzayniddinov7777@gmail.com>
2022-06-18 02:14:58 +0500

    fix bug

--- examples/10-broken-counter/src/Application.tsx
+++ examples/10-broken-counter/src/Application.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react';
 
+
 const Counter = () => {
   const [count, setCount] = useState(0);
 
-  useEffect(() => setTimeout(() => setCount(count + 1), 1000), [count]);
+  useEffect(() => { setTimeout(() => setCount(count + 1), 1000) }, [count]);
 
   return <main className="count">{count}</main>;
 };
