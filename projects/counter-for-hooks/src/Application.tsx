9806af41f9eb109aa3e53797834d32d21089280f
Fozil Zayn <fozilzayniddinov7777@gmail.com>
2022-06-19 15:47:21 +0500

    reducer with types

--- projects/counter-for-hooks/src/Application.tsx
+++ projects/counter-for-hooks/src/Application.tsx
@@ -1,17 +1,68 @@
+import { useReducer, useState } from "react";
+
+
+type BasicCounterAction = {
+  type: "INCREMENT" | "DECREMENT"
+}
+
+type SetCounterState = {
+  type: "SET",
+  payload: number
+}
+
+type CounterState = {
+  value: number
+}
+
+type CounterAction = BasicCounterAction | SetCounterState
+
+
+const reducer = (state: CounterState, action: CounterAction) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { value: state.value + 1 }
+    case "DECREMENT":
+      return { value: state.value - 1 }
+    case "SET":
+      return { value: action.payload }
+    default:
+      return state;
+
+  }
+}
+
+
 const Counter = () => {
+
+  const [state, dispatch] = useReducer(reducer, { value: 0 })
+  const [value, setValue] = useState(0);
+
+  const increment = () => dispatch({ type: "INCREMENT" })
+  const decrement = () => dispatch({ type: "DECREMENT" })
+  const reset = () => dispatch({ type: "SET", payload: 0 })
+
+  const set = (n: number) => dispatch({ type: "SET", payload: n })
+
+
+  const formSubmit = (e: any) => {
+    e.preventDefault()
+    set(value);
+
+  }
+
   return (
     <main className="Counter">
       <h1>Days Since Last Incident</h1>
-      <p className="count">0</p>
+      <p className="count">{state.value}</p>
       <section className="controls">
-        <button>Increment</button>
-        <button>Reset</button>
-        <button>Decrement</button>
+        <button onClick={increment}>Increment</button>
+        <button onClick={reset}>Reset</button>
+        <button onClick={decrement}>Decrement</button>
       </section>
       <section className="controls">
-        <form onSubmit={() => {}}>
+        <form onSubmit={(event) => { formSubmit(event) }}>
           <label htmlFor="set-to">Set Count</label>
-          <input id="set-to" type="number" />
+          <input id="set-to" type="number" onChange={e => setValue(+e.target.value)} />
           <input type="submit" />
         </form>
       </section>
